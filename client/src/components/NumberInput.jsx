f9f2894ca5fe39df2044468e3c0227d28c179b72
Mpendulo1 <mpendulokhozamk2@gmail.com>
2022-04-10 01:12:13 +0200

    💄 add border-bottom to form fields

--- client/src/components/NumberInput.jsx
+++ client/src/components/NumberInput.jsx
@@ -1,9 +1,11 @@
 
 function NumberInput({label, field, name, helperText, ...props}) {
     return(
-        <div className="flex flex-col justify-start items-start w-full py-2">
+        <div className="flex flex-col justify-start items-start w-full py-1">
         <label className="text-md font-semibold text-indigo-400 pb-1" htmlFor={field.name}>{label}</label>
-        <input className="bg-gray-200 w-full border-b-4 focus:border-indigo-400 focus:outline-none rounded-sm px-1 py-2 text-xl font-medium text-gray-600" type="number" {...field} {...props} />
+        <input className="bg-gray-200 w-full border-b-4 focus:border-indigo-400 focus:outline-none rounded-sm px-1 text-xl font-normal text-gray-500" 
+            type="number" {...field} {...props} 
+        />
         <p className="text-red-500">{helperText}</p>
         </div>
     );
--- client/src/components/SelectInput.jsx
+++ client/src/components/SelectInput.jsx
@@ -2,9 +2,9 @@
 function SelectInput({label, field, helperText, options, ...props}) {
   
   return (
-    <div className="flex flex-col justify-start items-start w-full py-2">
+    <div className="flex flex-col justify-start items-start w-full py-1">
         <label className="text-md font-semibold text-indigo-400 pb-1" htmlFor={field.name}>{label}</label>
-        <select className="bg-gray-200 w-full border-b-4 focus:border-indigo-400 focus:outline-none rounded-sm px-1 py-2 text-xl font-medium text-gray-700"  {...field} {...props} >
+        <select className="bg-gray-200 w-full border-b-4 focus:border-indigo-400 focus:outline-none rounded-sm px-1 text-xl font-normal text-gray-500"  {...field} {...props} >
         {
             options.map( op => (<option key={op} value={op} >{op}</option>) )
         }
