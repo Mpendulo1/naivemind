1399ed2b0566aca9f3ee54fd40764d7437644544
Mpendulo1 <mpendulokhozamk2@gmail.com>
2022-04-09 16:47:35 +0200

    ✏️ fix typo: replace id with key

--- client/src/components/SelectInput.jsx
+++ client/src/components/SelectInput.jsx
@@ -6,7 +6,7 @@ function SelectInput({question, id, options}) {
         <label htmlFor={id} className="block mb-2 text-sm font-medium text-indigo-400">{question}</label>
         <select id={id} name={id} className="bg-yellow-50 border border-indigo-300 text-indigo-500 text-sm rounded block w-full p-2 mb-2" >
             {
-                options.map( (option, index) => (<option id={index} value={option} >{option}</option>) )
+                options.map( (option, index) => (<option key={index} value={option} >{option}</option>) )
             }
         </select>
         </>
