0268bede8e400d5b8969bbc472d12a258928c64d
Mpendulo1 <mpendulokhozamk2@gmail.com>
2022-04-11 03:31:54 +0200

    💄 capitalize label text

--- client/src/components/NumberInput.jsx
+++ client/src/components/NumberInput.jsx
@@ -2,7 +2,7 @@
 function NumberInput({label, field, name, helperText, ...props}) {
     return(
         <div className="flex flex-col justify-start items-start w-full py-1">
-        <label className="text-md font-semibold text-indigo-400 pb-1" htmlFor={field.name}>{label}</label>
+        <label className="text-md font-semibold text-indigo-400 pb-1 capitalize" htmlFor={field.name}>{label}</label>
         <input className="bg-gray-200 w-full border-b-4 focus:border-indigo-400 focus:outline-none rounded-sm px-1 text-xl font-normal text-gray-500" 
             type="number" {...field} {...props} 
         />
--- client/src/components/SelectInput.jsx
+++ client/src/components/SelectInput.jsx
@@ -3,7 +3,7 @@ function SelectInput({label, field, helperText, options, ...props}) {
   
   return (
     <div className="flex flex-col justify-start items-start w-full py-1">
-        <label className="text-md font-semibold text-indigo-400 pb-1" htmlFor={field.name}>{label}</label>
+        <label className="text-md font-semibold text-indigo-400 pb-1 capitalize" htmlFor={field.name}>{label}</label>
         <select className="bg-gray-200 w-full border-b-4 focus:border-indigo-400 focus:outline-none rounded-sm px-1 text-xl font-normal text-gray-500"  {...field} {...props} >
         {
             options.map( op => (<option key={op} value={op} >{op}</option>) )
